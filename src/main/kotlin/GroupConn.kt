929e4a510bd515063dc42ea878d6469e6d16c6ba
LaoLittle <y1941803875@outlook.com>
2021-12-13 18:18:56 +0800

    添加消息撤回

--- src/main/kotlin/GroupConn.kt
+++ src/main/kotlin/GroupConn.kt
@@ -5,14 +5,22 @@ import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.contact.Contact.Companion.sendImage
 import net.mamoe.mirai.contact.nameCardOrNick
 import net.mamoe.mirai.event.GlobalEventChannel
-import net.mamoe.mirai.message.data.buildMessageChain
+import net.mamoe.mirai.event.ListeningStatus
+import net.mamoe.mirai.event.broadcast
+import net.mamoe.mirai.event.events.MessageRecallEvent.GroupRecall
+import net.mamoe.mirai.event.subscribeGroupMessages
+import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.utils.info
 import org.laolittle.plugin.groupconn.command.CloseConnection
-import org.laolittle.plugin.groupconn.command.Connect
 import org.laolittle.plugin.groupconn.command.List
-import org.laolittle.plugin.groupconn.model.ConnEvent
+import org.laolittle.plugin.groupconn.command.OpenConnection
+import org.laolittle.plugin.groupconn.model.ConnGroupMessageEvent
+import org.laolittle.plugin.groupconn.model.ConnGroupRecallEvent
+import org.laolittle.plugin.groupconn.utils.DrawMessage.getHeadImg
+import org.laolittle.plugin.groupconn.utils.DrawMessage.processMessageImg
 
 object GroupConn : KotlinPlugin(
     JvmPluginDescription(
@@ -26,14 +34,30 @@ object GroupConn : KotlinPlugin(
     @OptIn(ConsoleExperimentalApi::class, ExperimentalCommandDescriptors::class)
     override fun onEnable() {
         List.register()
-        Connect.register()
+        OpenConnection.register()
         CloseConnection.register()
-        logger.info { "Plugin loaded" }
-        GlobalEventChannel.subscribeAlways<ConnEvent> {
-            target.sendMessage(buildMessageChain {
+        logger.info { "跨群聊天初始化完成" }
+        GlobalEventChannel.subscribeGroupMessages {
+            "teea" {
+                subject.sendImage(processMessageImg(getHeadImg(sender)))
+            }
+        }
+        GlobalEventChannel.subscribeAlways<ConnGroupMessageEvent> {
+         val sentOutMessage = target.sendMessage(buildMessageChain {
                 add(sender.nameCardOrNick + "\n")
-                add(message)
+                add(message.toMessageChain())
             })
+            GlobalEventChannel.subscribe<GroupRecall> {
+                if ((messageIds.contentEquals(message.ids))&&(messageInternalIds.contentEquals(message.internalId))&&(messageTime == message.time)){
+                    val recallEvent = ConnGroupRecallEvent(sentOutMessage)
+                    recallEvent.broadcast()
+                    return@subscribe ListeningStatus.STOPPED
+                }
+                ListeningStatus.LISTENING
+            }
+        }
+        GlobalEventChannel.subscribeAlways<ConnGroupRecallEvent> {
+            targetMessage.recall()
         }
     }
 }
--- src/main/kotlin/model/ConnGroupMessageEvent.kt
+++ src/main/kotlin/model/ConnGroupMessageEvent.kt
@@ -3,13 +3,18 @@ package org.laolittle.plugin.groupconn.model
 import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.event.AbstractEvent
-import net.mamoe.mirai.message.data.Message
+import net.mamoe.mirai.message.MessageReceipt
+import net.mamoe.mirai.message.data.MessageChain
 
-class ConnEvent(
-    val message: Message,
+class ConnGroupMessageEvent(
+    val message: MessageChain,
     val sender: User,
     val group: Group,
     val target: Group
+) : AbstractEvent()
+
+class ConnGroupRecallEvent(
+    val targetMessage: MessageReceipt<Group>,
 ) : AbstractEvent()
 
 val activeGroups: MutableMap<Group, Group> = mutableMapOf()
