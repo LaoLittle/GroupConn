36989d71fedcef0ee8769db2caeaf59b4812dee8
LaoLittle <y1941803875@outlook.com>
2021-12-17 17:21:42 +0800

    删去不必要的判断

--- src/main/kotlin/command/List.kt
+++ src/main/kotlin/command/List.kt
@@ -2,8 +2,6 @@ package org.laolittle.plugin.groupconn.command
 
 import net.mamoe.mirai.console.command.CommandSenderOnMessage
 import net.mamoe.mirai.console.command.SimpleCommand
-import net.mamoe.mirai.console.command.getGroupOrNull
-import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.message.data.buildForwardMessage
 import org.laolittle.plugin.groupconn.GroupConn
@@ -14,12 +12,12 @@ object List : SimpleCommand(
 ) {
     @Handler
     suspend fun CommandSenderOnMessage<*>.handle() {
-        val groups = buildForwardMessage(if (subject is Group) subject!! else user!!) {
-            for ((i, group) in bot!!.groups.withIndex()) {
-                add(bot!!, PlainText("$i: ${group.name}(${group.id})"))
+        val groups = buildForwardMessage(fromEvent.subject) {
+            for ((i, group) in fromEvent.bot.groups.withIndex()) {
+                add(fromEvent.bot, PlainText("$i: ${group.name}(${group.id})"))
             }
         }
 
-        getGroupOrNull()?.sendMessage(groups)
+        fromEvent.subject.sendMessage(groups)
     }
 }
