f0e2691f60dd5e226825171c834307495d484374
LaoLittle <y1941803875@outlook.com>
2021-12-17 17:21:54 +0800

    未使用

---
+++ src/main/kotlin/GroupConnConfig.kt
@@ -0,0 +1,9 @@
+package org.laolittle.plugin.groupconn
+
+/*
+object GroupConnConfig : AutoSavePluginConfig("GroupConnConfig") {
+    @ValueDescription("消息记录额外经过的群聊 (群号)")
+    val middleGroup: Long? by value(null)
+}
+
+ */
--- src/main/kotlin/utils/DrawMessage.kt
+++ src/main/kotlin/utils/DrawMessage.kt
@@ -1,45 +1,32 @@
 package org.laolittle.plugin.groupconn.utils
 
-import net.mamoe.mirai.contact.User
-import java.awt.AlphaComposite
-import java.awt.Color
-import java.awt.RenderingHints
-import java.awt.geom.RoundRectangle2D
-import java.awt.image.BufferedImage
-import java.awt.image.BufferedImage.TYPE_INT_ARGB
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.net.URL
-import javax.imageio.ImageIO
-
-
+/*
 object DrawMessage {
-  fun processMessageImg(avatar: BufferedImage): InputStream {
-      val width = 300
-      val height = 200
-      val messageImage = BufferedImage(width, height, TYPE_INT_ARGB)
-      val bgImage = BufferedImage(500, 500, TYPE_INT_ARGB)
-      val g2 = messageImage.createGraphics()
-      g2.composite = AlphaComposite.Src
-      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-      g2.color = Color.WHITE
-      g2.fill(
-          RoundRectangle2D.Float(
-              0F, 0F, width.toFloat(), height.toFloat(),
-              50F, 50F
-          )
-      )
-      g2.dispose()
-      val bg2 = bgImage.createGraphics()
-      bg2.composite = AlphaComposite.Src
-      bg2.drawImage(messageImage, 150, 0, width, height, null)
-      bg2.drawImage(avatar, 0, 0, 105, 105, null)
-      bg2.dispose()
-      val os = ByteArrayOutputStream()
-      ImageIO.write(bgImage, "PNG", os)
-      return ByteArrayInputStream(os.toByteArray())
-  }
+    fun processMessageImg(avatar: BufferedImage): InputStream {
+        val width = 300
+        val height = 200
+        val messageImage = BufferedImage(width, height, TYPE_INT_ARGB)
+        val bgImage = BufferedImage(500, 500, TYPE_INT_ARGB)
+        val g2 = messageImage.createGraphics()
+        g2.composite = AlphaComposite.Src
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        g2.color = Color.WHITE
+        g2.fill(
+            RoundRectangle2D.Float(
+                0F, 0F, width.toFloat(), height.toFloat(),
+                50F, 50F
+            )
+        )
+        g2.dispose()
+        val bg2 = bgImage.createGraphics()
+        bg2.composite = AlphaComposite.Src
+        bg2.drawImage(messageImage, 150, 0, width, height, null)
+        bg2.drawImage(avatar, 0, 0, 105, 105, null)
+        bg2.dispose()
+        val os = ByteArrayOutputStream()
+        ImageIO.write(bgImage, "PNG", os)
+        return ByteArrayInputStream(os.toByteArray())
+    }
 
     fun getHeadImg(sender: User): BufferedImage {
         val targetSize = 105
@@ -62,3 +49,5 @@ object DrawMessage {
         return roundImage
     }
 }
+
+ */
